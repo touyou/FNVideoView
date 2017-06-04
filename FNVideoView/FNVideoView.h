baee7ecbb23fbfe69f79aa7d7fbb8292330dff0f
funzin <famistap@gmail.com>
2017-06-04 11:13:56 +0900

    Initial commit

---
+++ FNVideoView/FNVideoView.h
@@ -0,0 +1,19 @@
+//
+//  FNVideoView.h
+//  FNVideoView
+//
+//  Created by 中澤郁斗 on 2017/06/04.
+//  Copyright © 2017年 中澤郁斗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FNVideoView.
+FOUNDATION_EXPORT double FNVideoViewVersionNumber;
+
+//! Project version string for FNVideoView.
+FOUNDATION_EXPORT const unsigned char FNVideoViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FNVideoView/PublicHeader.h>
+
+
